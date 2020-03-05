b4b67597cd0c08856ee0cc1a8f4599088f91e47c
Little-prince-tamed-by-a-fox <61703293+Little-prince-tamed-by-a-fox@users.noreply.github.com>
2020-03-05 17:20:42 +0800

    有向图的DFS_邻接表.cpp
    图论

---
+++ 有向图的DFS_邻接表.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAXN 205
+#define MAXM 20005
+int fir[MAXN];
+bool vis[MAXN];
+int ecnt, ans, n, m;
+struct edge                 // 结构体存图
+{
+    int v, nxt;
+}ed[MAXM];
+void adde(int a, int b)        // 加边操作
+{
+    ed[++cnt].v=b, ed[ecnt].nxt = fir[a]=ecnt;
+}
+void dfs(int x)                  // 搜索
+{
+    vis[x]=1;
+    ans++;
+    for(int i=fir[x]; i; i = nxt[i])
+    {
+        int t = ed[i].v;
+        if(!vis[t])
+            dfs(t);
+    }
+}
+int main()
+{
+    int a, b, x;
+    scanf("%d %d", &n, &m);
+    for(register int i = 1; i <= m; i++)
+    {
+        scanf("%d%d", &a, &b);
+        adde(a, b)'
+    }
+    scanf("%d", &x);
+    dfs(x);
+    printf("%d\n", ans);
+    return 0;
+}
